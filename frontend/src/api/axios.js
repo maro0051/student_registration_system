82fb2a9784eb8b647efc778c893e5650669b351c
daniel maroy <danielmaroy8@gmail.com>
2026-06-07 20:43:28 -0400

    final 6

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
     baseURL:
-        "http://127.0.0.1:8000/api/"
+        "https://student-registration-system-1ej3.onrender.com/api/"
 
 });
 
